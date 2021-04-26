37db5b4fa0715def1e9f07038b8f747ff8f26de8
unknown <hazartilirot@gmail.com>
2021-04-27 01:20:39 +0300

    stage 2 - before routing

--- src/App.js
+++ src/App.js
@@ -1,30 +1,56 @@
 import React from "react";
 import Navbar from "./components/layout/Navbar";
+import Search from "./components/users/Search";
 import Users from "./components/users/Users";
-import axios from 'axios';
+import Alert from "./components/layout/Alert";
+import axios from "axios";
 import "./App.css";
 
 class App extends React.Component {
   state = {
     users: [],
-    loading: false
-  }
-  
-  async componentDidMount() {
-    this.setState({ loading: true });
-    
-    const res = await axios.get("https://api.github.com/users");
-    this.setState({ users: res.data, loading: false})
-  }
+    loading: false,
+    alert: null,
+  };
 
-  render = () => (
-    <div className="App">
-      <Navbar />
-      <div className="container">
-        <Users loading={this.state.loading} users={this.state.users}/>
-      </div>
-    </div>
-  );
+  searchUsers = async (text) => {
+    this.setState({loading: true});
+    const res = await axios.get("https://api.github.com/search/users", {
+      params: {
+        q: text,
+        client_id: process.env.REACT_APP_GITHUB_CLIENT_ID,
+        client_secret: process.env.REACT_APP_GITHUB_CLIENT_SECRET,
+      },
+    });
+    this.setState({users: res.data.items, loading: false, alert: null});
+  };
+
+  clearUsers = () => this.setState({users: [], loading: false, alert: null});
+
+  setAlert = (msg, type) => {
+    this.setState({alert: {msg, type}});
+    setTimeout(() => this.setState({alert: null}), 5000)
+  };
+
+  render = () => {
+    const {users, loading} = this.state;
+
+    return (
+        <div className="App">
+          <Navbar/>
+          <div className="container">
+            <Alert alert={this.state.alert}/>
+            <Search
+                searchUsers={this.searchUsers}
+                clearUsers={this.clearUsers}
+                showClear={users.length > 0}
+                setAlert={this.setAlert}
+            />
+            <Users loading={loading} users={users}/>
+          </div>
+        </div>
+    );
+  };
 }
 
 export default App;
